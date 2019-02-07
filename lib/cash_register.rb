a6c83e901243e70e6aed69adc9fadcc290548918
Sandesh Karki <karkisandesh@hotmail.com>
2019-02-07 04:40:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,9 +8,9 @@ class CashRegister
   
   def add_item(item, price, quantity = 0)
     @item = item 
-    @price = price 
+    @price = price
     @quantity = quantity
-    total += price + quantity
+    total = price + quantity
 
     
     
